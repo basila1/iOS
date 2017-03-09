accc72c6d96875fcf3aed75a710db4cd6ba8270e
basila1 <basila_n@outlook.com>
2017-03-09 14:00:10 -0800

    finished tipster

--- Tipster/Tipster/ViewController.swift
+++ Tipster/Tipster/ViewController.swift
@@ -17,23 +17,76 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipPercent1: UILabel!
     @IBOutlet weak var tipPercent2: UILabel!
     @IBOutlet weak var tipPercent3: UILabel!
+    
     @IBOutlet weak var tipAmount1: UILabel!
     @IBOutlet weak var tipAmount2: UILabel!
     @IBOutlet weak var tipAmount3: UILabel!
+    
     @IBOutlet weak var totalAmount1: UILabel!
     @IBOutlet weak var totalAmount2: UILabel!
     @IBOutlet weak var totalAmount3: UILabel!
     
-    @IBAction func tipSlider(_ sender: UISlider) {
+    @IBOutlet weak var customTipPercentageSlider: UISlider!
+    @IBOutlet weak var groupTextLabel: UILabel!
+    
+    
+    @IBAction func tipPercentageSlider(_ sender: UISlider) {
+        tipPercent1.text! = String(Int(sender.value) + 5) + "%"
+        tipPercent2.text! = String(Int(sender.value) + 10) + "%"
+        tipPercent3.text! = String(Int(sender.value) + 15) + "%"
+        calculateTip()
     }
     
+    func calculateTip() {
+        let tip = Float(customTipPercentageSlider.value)
+        let tipAmountA = Float(numLabel.text!)! * ((tip + 5)/100)
+        let tipAmountB = Float(numLabel.text!)! * ((tip + 10)/100)
+        let tipAmountC = Float(numLabel.text!)! * ((tip + 15)/100)
+        let perPersonA = (Float(numLabel.text!)! + tipAmountA)/round(groupSliderLabel.value)
+        let perPersonB = (Float(numLabel.text!)! + tipAmountB)/round(groupSliderLabel.value)
+        let perPersonC = (Float(numLabel.text!)! + tipAmountC)/round(groupSliderLabel.value)
+        
+        
+
+//        let totalA = tipAmountA + Float(numLabel.text!)!
+//        let totalB = tipAmountB + Float(numLabel.text!)!
+//        let totalC = tipAmountC + Float(numLabel.text!)!
+
+        tipAmount1.text! = String(tipAmountA)
+        tipAmount2.text! = String(tipAmountB)
+        tipAmount3.text! = String(tipAmountC)
+        
+        totalAmount1.text! = String(perPersonA)
+        totalAmount2.text! = String(perPersonB)
+        totalAmount3.text! = String(perPersonC)
+
+    }
+
+    @IBOutlet weak var groupSliderLabel: UISlider!
+    
+    
     @IBAction func groupSlider(_ sender: UISlider) {
+
+        calculateTip()
+        let groupSize = Int(sender.value)
+        groupTextLabel.text = "Group Size: \(groupSize)"
+        
     }
     
     
     //function for buttons pressed
     @IBAction func numberPressed(_ sender: UIButton) {
         print(sender.tag)
+        if sender.tag < 10 {
+            numLabel.text = numLabel.text! + String(sender.tag)
+        }
+        if sender.tag == 11 {
+            numLabel.text = numLabel.text! + String(".")
+            sender.isEnabled = false
+        }
+        if sender.tag == 10 {
+            numLabel.text = String("")
+        }
     }
     
     
