09e85b2e42ad25b2b2b60e35a73841d8753c78bf
basila1 <basila_n@outlook.com>
2017-03-25 23:57:24 -0700

    contact list added

---
+++ ContactList/ContactList/ContactListTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Basila Nathan on 3/22/17.
+//  Copyright © 2017 Basila Nathan. All rights reserved.
+//
+
+import UIKit
+
+class ContactListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+}
+
