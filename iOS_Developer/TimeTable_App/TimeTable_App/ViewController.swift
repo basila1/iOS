9f4c3f484022d4598f472ff0a1aabae62c47f709
basila1 <basila_n@outlook.com>
2017-06-27 17:44:42 -0700

    TimeTable App finished

--- iOS_Developer/TimeTable_App/TimeTable_App/ViewController.swift
+++ iOS_Developer/TimeTable_App/TimeTable_App/ViewController.swift
@@ -8,8 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    
 
+    @IBAction func sliderChanged(_ sender: Any) {
+        
+        tableView.reloadData()
+        
+    }
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = String((Int(slider.value * 20)) * (indexPath.row + 1))
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
